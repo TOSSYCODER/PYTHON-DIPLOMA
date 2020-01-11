f538612b5e7eff021257659799b54a3b47510035
Tauseef Shaikh <48127782+TOSSYCODER@users.noreply.github.com>
2020-01-11 16:51:29 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,98 @@
+# dict = {
+    # 'name' : "tauseef",
+    # 'age' : 19,
+    # 'phone': [222222, 555555]
+# }
+# 
+# dict['age'] = 20
+# 
+# dict['gender'] = 'male'
+# 
+# 
+# print(dict)
+# 
+# print(dict['phone'][1])
+# 
+#FORLOOP:
+#
+# for i in range(0,5,1): 
+    # print (i)
+# 
+# print('hallo')
+
+#l1 = [1,2,3,4,5,6,7]
+
+# for i in range(0,5,1):
+    # print(l1[i])
+    
+# for i in l1:
+    # print(i)    
+
+# for i in range(0,(len(l1)-2),1):
+    # print (l1[i])
+#
+#WHILE LOOP:
+#
+# a=2
+# 
+# while(a<10): 
+    # print('INSIDE LOOP')
+    # a = a+2
+# 
+# print ("OUT OF LOOP")
+#
+#TASK(FIBONACCI SERIES)
+#
+# a = 0
+# print (a)
+# b = a + 1
+# print(b)
+# i=0
+# while(i<10):
+    # c=a+b
+    # print (c)
+    # a = b
+    # b = c
+    # i = i+1
+# 
+#FUNCTIONS
+#
+# def add(num1, num2):
+    # sum = num1+num2
+    # return sum
+# 
+# def sub(num1, num2):
+    # sum = num1-num2
+    # return sum
+# 
+# def multi(num1, num2):
+    # sum = num1*num2
+    # return sum
+# 
+# def div(num1, num2):
+    # sum = num1/num2
+    # return sum
+# 
+# print(add(15,20))
+# 
+# print(sub(15,20))
+# 
+# print(multi(15,20))
+# 
+# print(div(15,20))
+#
+#TASK(FIBO USINF FUNC)
+#
+# def fibo(n):
+    # a,b = 0,1
+    # print (a)
+    # print(b)
+    # i=2
+    # while(i<n):
+        # c=a+b
+        # print (c)
+        # a = b
+        # b = c
+        # i = i+1
+# 
+# fibo(5)
